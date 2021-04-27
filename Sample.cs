2a0ceb6f9372617650e45a33aa61283542175e36
KRNagarjun <kr.nagarjun@ltts.com>
2021-04-27 12:46:29 +0530

    Classlibrary

---
+++ Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleClassLibrary
+{
+    class Sample
+    {
+       static void FunctionGeneral()
+        {
+            Console.WriteLine("Hello From Second Class");
+        }
+    }
+}
